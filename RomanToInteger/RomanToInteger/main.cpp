7e960c96408613de9c48df2b3a6c49953c0ad1d3
zhuwenzhen <wenzhen.zwz@gmail.com>
2017-05-26 00:01:23 -0500

    daily practice

---
+++ RomanToInteger/RomanToInteger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RomanToInteger
+//
+//  Created by Wenzhen Zhu on 5/25/17.
+//  Copyright © 2017 Wenzhen Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ TrappingRainWater/TrappingRainWater/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TrappingRainWater
+//
+//  Created by Wenzhen Zhu on 5/24/17.
+//  Copyright © 2017 Wenzhen Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
